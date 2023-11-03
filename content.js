a2563f855c98b0b3e2868ca06a080307b81579a6
savmg <skbmasale941@gmail.com>
2023-11-03 14:59:07 +0530

    Added a function

--- content.js
+++ content.js
@@ -1,27 +1,55 @@
-setTimeout(() => {
-  console.log("hey I am working");
-  const videos = document.querySelectorAll("ytd-rich-item-renderer");
-  console.log(videos);
+function modifyThumbnails() {
+  // Removing the first and foremost ad-slot
+  const firstAdSlot = document.querySelector(
+    ".ytd-in-feed-ad-layout-renderer"
+  );
+  firstAdSlot && firstAdSlot.remove();
 
-  videos.forEach((e) => {
-    const img = e.querySelector("img");
-    const imgParent = img.parentElement;
+  const videoPreviewScreen = document.querySelectorAll(
+    "ytd-rich-item-renderer"
+  );
+  const videoTitles = document.querySelectorAll("#video-title");
+
+  videoPreviewScreen.forEach((e, i) => {
+    const thumbnail = e.querySelector("img");
+    const thumbnailParent = thumbnail.parentElement;
+
+    // making title center
+    thumbnailParent.style.display = "flex";
+    thumbnailParent.style.height = "100%";
+    thumbnailParent.style.justifyContent = "center";
+    thumbnailParent.style.alignItems = "center";
 
     // Create an h1 element
     const h1 = document.createElement("h1");
+    h1.style.textAlign = "center";
+
+    // adding the title to h1
+    h1.textContent = videoTitles[i].innerText;
 
     // Adding the class
     h1.classList.add("iamTheVideoTitle");
 
-    h1.textContent = "This is the image title";
-
     // Append the h1 element to the parent
-    imgParent.appendChild(h1);
+    thumbnailParent.appendChild(h1);
 
-    // Adding text to title
-    h1.innerText = "This is the video title";
-
-    // Remove the img element
-    img.remove();
+    // Remove the thumbnail element
+    thumbnail.remove();
   });
-}, 5000);
+}
+// window.addEventListener("scroll", () => {
+//   const scrollPosition = window.scrollY;
+//   const totalHeight =
+//     document.documentElement.scrollHeight - window.innerHeight;
+//   const scrollPercentage = (scrollPosition / totalHeight) * 100;
+
+//   if (scrollPercentage > 80) {
+//   }
+//   return;
+// });
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    modifyThumbnails();
+  }, 5000);
+});
